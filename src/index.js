373ab760a4be146f175800fe05ede0f3d4173c0d
seniormike <ma.puentes@uniandes.edu.co>
2020-10-23 00:22:46 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Formulario from "./comp/form";
 import "bootstrap/dist/css/bootstrap.css";
-
 const fields = [
   { type: "text", name: "Nombre", placeholder: "Nombre", validators: { required: true, minLength: 5, maxLength: 20 } },
   { type: "text", name: "Apellido", placeholder: "Apellido", validators: { required: true, minLength: 5, maxLength: 20 } },
   { type: "email", name: "Correo", placeholder: "Correo", validators: { required: true, minLength: 5, maxLength: 20 } },
 ];
-
 const initialValues = { Nombre: "", Apellido: "", Correo: "" };
-
 ReactDOM.render(<Formulario fields={fields} initialValues={initialValues} />, document.getElementById("root"));
